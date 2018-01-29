a0659155a56661893da8f53ca29c83516349570f
Amit Choukroun <amit@mtrn.co.il>
2018-01-29 13:36:59 +0200

    add post put and delete endpoints

--- src/main/java/com/openu/forum/TopicController.java
+++ src/main/java/com/openu/forum/TopicController.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +21,30 @@ public class TopicController {
 	@GetMapping("/topics")
 	public List<Topic> getAll() {
 		List<Topic> list = new ArrayList<Topic>();
+		
 		repository.findAll().forEach(list::add);
 		return list;
 	}
 	
+	@PostMapping("/topics")
+	public Topic addTopic(@RequestBody Topic t) {
+		if(t.getId() != 0)
+			return null;
+		
+		return repository.save(t);
+	}
 	
+	@PutMapping("/topics")
+	public Topic editTopic(@RequestBody Topic t) {
+		if(t.getId() == 0)
+			return null;
+		
+		return repository.save(t);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable(value = "id") Long id) {
+		
+		repository.deleteById(id);
+	}
 }
