f865e9da15e32ef5286dae49af04fbdc98864679
Amit Choukroun <amit@mtrn.co.il>
2018-03-12 11:17:28 +0200

    fix user login to work only on registered users

--- src/main/java/com/openu/forum/WebSecurityConfig.java
+++ src/main/java/com/openu/forum/WebSecurityConfig.java
@@ -38,7 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
  protected void configure(HttpSecurity http) throws Exception {
    http
    .authorizeRequests()
-   .antMatchers(HttpMethod.POST, "/users").permitAll()
+   .antMatchers(HttpMethod.POST, "/api/users").permitAll()
+   .antMatchers(HttpMethod.GET, "/api/users/me").authenticated()
    .antMatchers("/topics")
    .authenticated()
   .anyRequest()
